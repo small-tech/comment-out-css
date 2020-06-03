af246b4a06dcdc5e25d369c8e331e9b94742ece6
Aral Balkan <mail@ar.al>
2020-06-03 16:35:08 +0100

    Change the extension id

--- src/extension.ts
+++ src/extension.ts
@@ -5,17 +5,12 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Comment Out CSS is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('small-tech.comment-out-css', () => {
-    // The code you place here will be executed every time your command is executed
-
+  let disposable = vscode.commands.registerCommand('comment-out-css', () => {
     const editor = vscode.window.activeTextEditor;
 
     if (editor) {
@@ -105,5 +100,5 @@ function countMatches(regExpMatchResult: RegExpMatchArray | null): number {
   return regExpMatchResult === null ? 0 : regExpMatchResult.length;
 }
 
-// this method is called when your extension is deactivated
+// This method is called when the extension is deactivated.
 export function deactivate() {}
